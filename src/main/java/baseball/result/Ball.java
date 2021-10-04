943dfefa9230a075ce3adfb7c2aa9970a1e85b4d
songseongbin <ppsosqq@gmail.com>
2021-10-04 09:42:35 +0900

    feat(Ball.java): 정답과 입력한 숫자가 다른 자리에 있는 경우 볼
    - Ball 일급 객체 추가

---
+++ src/main/java/baseball/result/Ball.java
@@ -0,0 +1,41 @@
+package baseball.result;
+
+import java.util.List;
+
+import baseball.BaseBallNumber;
+
+public class Ball {
+	private Integer count;
+
+	private Ball(Integer count) {
+		this.count = count;
+	}
+
+	public static Ball countOf(BaseBallNumber answerNumber, BaseBallNumber playerNumber) {
+		List<Integer> answerNumbers = answerNumber.toDigitList();
+		List<Integer> playerNumbers = playerNumber.toDigitList();
+
+		int count = 0;
+		for (int i = 0; i < 3; i++) {
+			count += addOneIfContainExceptEqualsIndex(answerNumbers, playerNumbers.get(i), i);
+		}
+
+		return new Ball(count);
+	}
+
+	private static int addOneIfContainExceptEqualsIndex(List<Integer> answerNumbers, Integer number, int exceptIdx) {
+		if (!answerNumbers.contains(number)) {
+			return 0;
+		}
+
+		return addOneIfNotEquals(answerNumbers.get(exceptIdx), number);
+	}
+
+	private static int addOneIfNotEquals(Integer first, Integer second) {
+		return !first.equals(second) ? 1 : 0;
+	}
+
+	public Integer getCount() {
+		return count;
+	}
+}
---
+++ src/test/java/baseball/result/BallTest.java
@@ -0,0 +1,44 @@
+package baseball.result;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import baseball.BaseBallNumber;
+
+class BallTest {
+	@Test
+	void 숫자가_모두_포함된_경우() {
+		Ball ball = Ball.countOf(BaseBallNumber.valueOf(523), BaseBallNumber.valueOf(352));
+
+		assertThat(ball.getCount()).isEqualTo(3);
+	}
+
+	@Test
+	void 숫자가_두개만_포함된_경우() {
+		Ball ball = Ball.countOf(BaseBallNumber.valueOf(487), BaseBallNumber.valueOf(758));
+
+		assertThat(ball.getCount()).isEqualTo(2);
+	}
+
+	@Test
+	void 숫자가_한개만_포함된_경우() {
+		Ball ball = Ball.countOf(BaseBallNumber.valueOf(459), BaseBallNumber.valueOf(235));
+
+		assertThat(ball.getCount()).isEqualTo(1);
+	}
+
+	@Test
+	void 숫자가_같은_자리에_포함된_경우1() {
+		Ball ball = Ball.countOf(BaseBallNumber.valueOf(459), BaseBallNumber.valueOf(459));
+
+		assertThat(ball.getCount()).isEqualTo(0);
+	}
+
+	@Test
+	void 숫자가_같은_자리에_포함된_경우2() {
+		Ball ball = Ball.countOf(BaseBallNumber.valueOf(459), BaseBallNumber.valueOf(549));
+
+		assertThat(ball.getCount()).isEqualTo(2);
+	}
+}
