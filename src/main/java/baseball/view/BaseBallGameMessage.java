3c26ecf7e7a4e7c84b5260220ef007213ad607d4
songseongbin <ppsosqq@gmail.com>
2021-10-06 02:06:05 +0900

    fix(BaseBallGameMessage.java): 스트라이크, 볼 메시지 수정
    - 스트라이크 메시지 공백 제거
    - 볼 메시지 공백 제거

--- src/main/java/baseball/view/BaseBallGameMessage.java
+++ src/main/java/baseball/view/BaseBallGameMessage.java
@@ -2,8 +2,8 @@ package baseball.view;
 
 public enum BaseBallGameMessage {
 	START("숫자를 입력해주세요 : "),
-	STRIKE("%d 스트라이크 "),
-	BALL("%d 볼"),
+	STRIKE("%d스트라이크 "),
+	BALL("%d볼"),
 	NOTHING("낫싱"),
 	ANSWER("3개의 숫자를 모두 맞히셨습니다! 게임 끝"),
 	CONTINUE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
